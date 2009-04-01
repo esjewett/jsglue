8e65bee47715ae3632fa62fc426f56be04e140dd
Ethan Jewett <esjewett@gmail.com>
2009-04-01 18:17:39 -0500

    Posting to Tarpipe

--- handler.rb
+++ handler.rb
@@ -1,19 +1,42 @@
+require 'net/http'
+require 'uri'
+
 class Handler
   
   def initialize
-    @stack = Array.new
+    @boundary = "TiDHew86xk"
   end
   
   def call(env)
-    @stack << env
+    
+    @target = env['QUERY_STRING'].to_s.slice(/url=[^&]*&?/)
+    @target.chomp!('&')
+    @target.reverse!.chomp!('=lru').reverse!
+                              
+    url = URI.parse('http://api.tarpipe.net/1.0/?key=f9d8e2df8b7ba57a4dd7e490b60d961d')
+    req = Net::HTTP::Post.new(url.path + '?' + url.query)
+    req.body = encode_multipartformdata({'title'=>'Test title', 'body'=>'Test body'})
+    req.set_content_type('multipart/form-data; boundary=' + @boundary)
+    res = Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
     
     [
       200,          # Status code
       {             # Response headers
         'Content-Type' => 'text/html'
       },
-      [env.to_s]        # Response body
+      [URI.unescape(url.path + '?' + url.query) + '-' + res.body]       # Response body 
     ]
   end
   
+  def encode_multipartformdata(parameters = {})
+    ret = String.new
+    parameters.each do |key, value|
+      unless value.empty?
+        ret << "\r\n--" << @boundary << "\r\n"
+        ret << "Content-Disposition: form-data; name=\"#{key}\"\r\n\r\n"
+        ret << value
+      end
+    end
+    ret << "\r\n--" << @boundary << "--\r\n"
+  end 
 end
